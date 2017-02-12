ca3604a6c6e97b8c5fbccd9b1916b1bb6aae8103
Alex Flores <floresalexx@gmail.com>
2017-02-11 20:05:54 -0800

    zip2arrays

---
+++ zip_2_arrays.js
@@ -0,0 +1,33 @@
+//Given 2 sorted arrays return 2nd array
+
+function zip_arrays(Aarray, Barray) {
+  var k = Aarray.length-1; //Aarray[k]
+  var h = Barray.length-1; //here's where the largest #'s will be placed
+  var i = h-k-1; //
+    do {
+       // console.log(i);
+       if (Barray[i] < Aarray[k]) {
+         Barray[h] = Aarray[k];
+         h--;
+         k--;
+       }
+       if (Barray[i] > Aarray[k]) {
+         Barray[h] = Barray[i];
+         h--;
+         i--;
+       }
+       else if (Barray[i] == Aarray[k]) {
+         Barray[i] = Aarray[h];
+         h--;
+         i--;
+         Aarray[k] = Barray[h];
+         h--;
+         k--;
+       }
+     }
+    while(h > i) {
+  }
+
+  return Barray;
+}
+console.log(zip_arrays( [5, 7, 10], [1, 6, 11, "", "", ""] ));
